531841ee761f9cf6aca9bb4ab95c6e57e6c6d89c
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-06-06 22:38:36 +0800

    chore: Setup working directory

---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Gerry Low on 2020-06-06.
+//  Copyright © 2020 Gerry Low. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
