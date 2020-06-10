fe31efb976d544b449dbb5c96d5780f0dadd1ae7
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-06-10 15:48:19 +0800

    feat: Implement fetchedResultsController to fetch and display photos if the user taps a pin that already have a photo album downloaded

--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -41,6 +41,7 @@ class PhotoAlbumViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = false
+        setupFetchedResultsController()
         registerCollectionViewCells()
         flowLayoutAdjustment(width: collectionView.frame.size.width)
         loadPin()
@@ -67,19 +68,43 @@ class PhotoAlbumViewController: UIViewController {
         mapView.showAnnotations([annotation], animated: true)
     }
     
+    fileprivate func setupFetchedResultsController() {
+        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+        let predicate = NSPredicate(format: "pin == %@", pin)
+        fetchRequest.predicate = predicate
+        let sortById = NSSortDescriptor(key: "id", ascending: true)
+        fetchRequest.sortDescriptors = [sortById]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: "lat\(pin.latitude)lon\(pin.longitude)-photos")
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
 }
 
 extension PhotoAlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return fetchedResultsController.sections?[0].numberOfObjects ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
-        cell.setImage(UIImage(named: "PhotoPlaceholder")!)
+        let photo = fetchedResultsController.object(at: indexPath)
+        if let imageData = photo.image, let image = UIImage(data: imageData) {
+            cell.setImage(image)
+        } else {
+            cell.setImage(UIImage(named: "PhotoPlaceholder")!)
+        }
         return cell
     }
+    
+}
+
+extension PhotoAlbumViewController: NSFetchedResultsControllerDelegate {
     
 }
 
